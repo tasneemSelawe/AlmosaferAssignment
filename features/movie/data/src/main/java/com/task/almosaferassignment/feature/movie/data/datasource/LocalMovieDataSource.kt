fb8ac42c1a6a87f38d7b33345dca7002e6a8938a
Tasneem <tasnim@Ahmeds-MacBook-Pro.local>
2023-05-21 01:22:29 +0300

    feat:[DONE]-add feature module

---
+++ features/movie/data/src/main/java/com/task/almosaferassignment/feature/movie/data/datasource/LocalMovieDataSource.kt
@@ -0,0 +1,9 @@
+package com.task.almosaferassignment.feature.movie.data.datasource
+
+import dagger.Reusable
+import javax.inject.Inject
+
+@Reusable
+internal class LocalMovieDataSource @Inject constructor() {
+
+}

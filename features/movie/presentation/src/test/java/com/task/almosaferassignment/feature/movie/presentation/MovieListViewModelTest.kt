032d3bde64a24ca78a55422d6e69326c70aff89e
Tasneem <tasnim@Ahmeds-MacBook-Pro.local>
2023-05-22 13:12:22 +0300

    test:[DONE]-implement movie list view modle unit test

--- features/movie/presentation/src/test/java/com/task/almosaferassignment/feature/movie/presentation/MovieListViewModelTest.kt
+++ features/movie/presentation/src/test/java/com/task/almosaferassignment/feature/movie/presentation/MovieListViewModelTest.kt
@@ -1,98 +1,76 @@
 package com.task.almosaferassignment.feature.movie.presentation
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.Observer
-import com.task.almosaferassignment.core.errorhandler.exception.RemoteException
-import com.task.almosaferassignment.core.errorhandler.model.HttpErrorBodyModel
 import com.task.almosaferassignment.feature.movie.domain.entity.Movie
 import com.task.almosaferassignment.feature.movie.domain.usecase.GetMovieListUseCase
+import com.task.almosaferassignment.feature.movie.presentation.movielist.MovieListViewModel
+import io.mockk.coEvery
+import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.runTest
-import org.junit.Assert.assertEquals
-import org.junit.Before
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.Mock
-import org.mockito.Mockito
-import org.mockito.MockitoAnnotations
 
 
-@ExperimentalCoroutinesApi
-internal class MovieListViewModelTest {
+@OptIn(ExperimentalCoroutinesApi::class)
+class MovieListViewModelTest2 {
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    private lateinit var viewModel: MovieListViewModel
+    private val getMovieListUseCase: GetMovieListUseCase = mockk(relaxed = true)
 
-    @Mock
-    private lateinit var getMovieListUseCase: GetMovieListUseCase
-
-    @Before
-    fun setup() {
-        MockitoAnnotations.openMocks(this)
-        viewModel = MovieListViewModel(getMovieListUseCase)
-    }
+    private val viewModel: MovieListViewModel =
+        MovieListViewModel(getMovieListUseCase = getMovieListUseCase)
 
     @Test
-    fun `test loading flow`() = runBlockingTest {
-        val loadingFlowValue = true
-        val loadingFlowMock = MutableStateFlow(loadingFlowValue)
-        val viewModelSpy = Mockito.spy(viewModel)
-        Mockito.doReturn(loadingFlowMock).`when`(viewModelSpy).loadingFlow
-
-        val actualLoadingFlowValue = viewModelSpy.loadingFlow.first()
-
-        assertEquals(loadingFlowValue, actualLoadingFlowValue)
-    }
-
-
-    @Test
-    fun `test movie list loading`() = runTest {
-
-        val mockMovieList = listOf(
-            Movie(
-                adult = true,
-                backdropPath = "",
-                genreIds = listOf(),
-                id = 0,
-                originalLanguage = "",
-                originalTitle = "",
-                overview = "",
-                popularity = 0.0,
-                posterPath = "",
-                releaseDate = "",
-                title = "",
-                video = true,
-                voteAverage = 0.0,
-                voteCount = 0
-            )
+    fun `test that loading flow value is true when getMoviesList is called`() = runTest {
+        //given
+        coEvery { getMovieListUseCase(any()) } returns listOf(
+            getMockedMovie(),
+            getMockedMovie(),
+            getMockedMovie()
         )
 
-        Mockito.`when`(getMovieListUseCase.invoke("popularity.desc")).thenReturn(mockMovieList)
-        Mockito.`when`(getMovieListUseCase.invoke("vote_average.desc")).thenReturn(listOf())
-        Mockito.`when`(getMovieListUseCase.invoke("revenue.desc")).thenReturn(null)
-
+        //when
         viewModel.getMovieList()
 
-        assertEquals(mockMovieList, viewModel.popularMovieList.firstOrNull())
-        assertEquals(listOf<Movie>(), viewModel.topRatedMovieList.firstOrNull())
-        assertEquals(null, viewModel.revenueMovieList.firstOrNull())
+        //then
+        Assert.assertEquals(true, viewModel.loadingFlow.first())
     }
+
 
     @Test
-    fun `test error flow`() = runTest {
-        val exception = Exception("Test Exception")
-        Mockito.`when`(getMovieListUseCase.invoke("popularity.desc")).thenThrow(exception)
+    fun `test that list flows flow is empty when getMoviesList returns empty list`() =
+        runTest {
+            //given
+            coEvery { getMovieListUseCase(any()) } returns listOf()
 
-        viewModel.getMovieList()
+            //when
+            viewModel.getMovieList()
 
-        assertEquals(exception, viewModel.errorFlow.firstOrNull())
+            //then
+            Assert.assertEquals(0, viewModel.popularMovieList.value?.size)
+            Assert.assertEquals(0, viewModel.topRatedMovieList.value?.size)
+            Assert.assertEquals(0, viewModel.revenueMovieList.value?.size)
+        }
 
-    }
-
+    private fun getMockedMovie() = Movie(
+        adult = false,
+        backdropPath = "",
+        genreIds = listOf(),
+        id = 1,
+        originalLanguage = "",
+        originalTitle = "",
+        overview = "",
+        popularity = 1.0,
+        posterPath = "",
+        releaseDate = "",
+        title = "",
+        video = false,
+        voteAverage = 1.0,
+        voteCount = 1
+    )
 }

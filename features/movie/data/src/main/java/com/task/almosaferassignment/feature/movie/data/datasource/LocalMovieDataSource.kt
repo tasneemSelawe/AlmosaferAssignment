5bb303c04327ed00bdcc219a1117e66571bfc6ea
Tasneem <tasnim@Ahmeds-MacBook-Pro.local>
2023-05-23 16:36:07 +0300

    remove un usesd code , update README file

--- features/movie/data/src/main/java/com/task/almosaferassignment/feature/movie/data/datasource/LocalMovieDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.task.almosaferassignment.feature.movie.data.datasource
-
-import dagger.Reusable
-import javax.inject.Inject
-
-@Reusable
-internal class LocalMovieDataSource @Inject constructor() {
-
-}
